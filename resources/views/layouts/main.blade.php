30bb4f63138ceea6421d9ec057d781d8f80d7a7b
Azzam Fajro <muhammadazzamfajro@gmail.com>
2025-10-11 17:07:49 +0700

    malas memberi nama pokoknya bagian navbar sama beranda

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />  {{-- buat accordion di halaman SPMB --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
 
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
@@ -23,20 +23,59 @@
     <header class="navbar">
         <div class="container">
             <a href="/" class="navbar-logo">
-                <img src="{{ asset('img/logo.png') }}" alt="Logo SMKN 1 Bangil">
+                <img src="{{ asset('img/logo-navbar.png') }}" alt="Logo SMKN 1 Bangil">
             </a>
+
             <nav class="navbar-nav">
                 <ul>
                     <li><a href="{{ route('beranda') }}">Beranda</a></li>
-                    <li><a href="{{ route('profil') }}">Profil</a></li>
-                    <li><a href="{{ route('jurusan') }}">Jurusan</a></li>
-                    <li><a href="{{ route('ekstrakurikuler') }}">Ekstrakurikuler</a></li>
-                    <li><a href="{{ route('berita') }}">Berita Sekolah</a></li>
-                    <li><a href="{{ route('galeri') }}">Galeri</a></li>
-                    <li><a href="#">GTK & SISWA</a></li>
-                    <li><a href="{{ route('spmb') }}" class="btn btn-primary">SPMB</a></li>
+                    <!-- Dropdown Profil -->
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle">Profil</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{ route('profil') }}">Profil</a></li>
+                            <li><a href="{{ route('jurusan') }}">Jurusan</a></li>
+                            <li><a href="{{ route('ekstrakurikuler') }}">Ekstrakurikuler</a></li>
+                        </ul>
+                    </li>
+
+                    <!-- Dropdown Berita Sekolah -->
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle">Berita Sekolah</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{ route('berita') }}" class="dropdown-toggle">Berita Sekolah</a></li>
+                            <li><a href="{{ route('galeri') }}">Galeri</a></li>
+                            <li><a href="#">Pengumuman</a></li>
+                            <li><a href="#">Kegiatan</a></li>
+                        </ul>
+                    </li>
+
+                    <!-- Dropdown GTK & SISWA -->
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle">Gtk & Siswa</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Data Pendidik</a></li>
+                            <li><a href="#">Data Tenaga Kependidikan</a></li>
+                            <li><a href="#">Data Siswa</a></li>
+                        </ul>
+                    </li>
+                    <!-- Dropdown fitur & app -->
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle">Fitur & App</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Smart BKK</a></li>
+                            <li><a href="#">Sertifikat Profesi</a></li>
+                            <li><a href="#">CBT Online</a></li>
+                            <li><a href="#">Peta Situs</a></li>
+                        </ul>
+                    </li>
+
+
+                    <li><a href="{{ route('spmb') }}" class="spmb-btn">SPMB</a></li>
                 </ul>
+
             </nav>
+
             <div class="navbar-extra">
                 <form action="#" class="search-form">
                     <input type="search" placeholder="Search">
@@ -46,6 +85,7 @@
         </div>
     </header>
 
+
     <main>
         @yield('content')
     </main>
@@ -54,12 +94,13 @@
         <div class="container">
             <div class="footer-top">
                 <div class="footer-about">
-                    <img src="{{ asset('img/footer/smk1bgl__1_-removebg-preview.png') }}" alt="Logo Footer SMKN 1 Bangil" class="footer-logo">
+                    <img src="{{ asset('img/footer/smk1bgl__1_-removebg-preview.png') }}"
+                        alt="Logo Footer SMKN 1 Bangil" class="footer-logo">
                     <div class="social-links">
                         <a
                             href="https://www.instagram.com/smknegeri1bangil?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><i
                                 class="fab fa-instagram"></i> Instagram</a>
-                        <a href="#"><i class="fab fa-facebook"></i> Facebook</a>
+                        <a href="https://www.facebook.com/SMKN1BANGIL"><i class="fab fa-facebook"></i> Facebook</a>
                         <a href="https://youtube.com/@nesabatv?si=WKsjdBl-HNJeBO7c"><i class="fab fa-youtube"></i>
                             YouTube</a>
                     </div>
@@ -102,7 +143,8 @@
         </div>
     </footer>
 
-    @yield('scripts') <!-- scripts accordion SPMB -->
+
+    @yield('script')
 </body>
 
 </html>
