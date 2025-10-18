e6a7097295b34bcbbc69ce16f2e1bd4406593c83
Sahrul Ramadhan Hardiansyah <sahrulrmdh3@gmail.com>
2025-10-18 11:21:51 +0700

    Update favicon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Admin Dashboard') - SMKN 1 Bangil</title>
+
+    <link rel="icon" href="{{ asset('img/favicon.png') }}" type="image/png">
     
     {{-- Memuat Font --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
